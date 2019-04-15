3377a39dc8943663c711e3df13fae4fd6906323e
Shih-Po Wang <shihpo@gmail.com>
2019-04-15 09:16:53 +0000

    HexViewerComponent support char viewing

--- src/components/HexViewerComponent.vue
+++ src/components/HexViewerComponent.vue
@@ -16,16 +16,16 @@
     </v-flex>
     <v-flex d-flex xs8>
       <v-card>
-        <v-card-text>
-          {{ row.hexs }}
-        </v-card-text>
+        <v-card-text
+          style="white-space: pre-wrap; word-wrap: break-word;"
+        >{{ row.hexs }}</v-card-text>
       </v-card>
     </v-flex>
     <v-flex d-flex>
       <v-card>
-        <v-card-text>
-          {{ row.chars }}
-        </v-card-text>
+        <v-card-text
+          style="white-space: pre-wrap; word-wrap: break-word;"
+        >{{ row.chars }}</v-card-text>
       </v-card>
     </v-flex>
   </v-layout>
@@ -90,12 +90,15 @@ export default {
         for(let j = 0; j < _str.length; j++) {
           hexs += _str[j].toUpperCase()
 
-        /* TODO fixme
-        if(j & 1) {
-          console.log('hex:' + _str.substring(j-1, j + 1), ',char:' + String.fromCharCode(parseInt(_str.substring(j - 1, j + 1), 16)))
-          chars += String.fromCharCode(parseInt(_str.substring(j - 1, j + 1), 16))
-        }
-				*/
+          if(j & 1) {
+            let charCode = parseInt(_str.substr(j - 1, 2), 16)
+            if(charCode >= 0x20 && charCode <= 0x7e) {
+              console.log(String.fromCharCode(charCode))
+              chars += String.fromCharCode(charCode)
+            }
+            else
+              chars += '?'
+          }
 
           if((j + 1) % 8 == 0) {
             hexs += ' '
