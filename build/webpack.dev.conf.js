f542aaee5949c0fa8ef9686bb39d9e180785321e
Shih-Po Wang <shihpo@gmail.com>
2019-04-25 00:27:00 +0000

    update webpack setting, using poll

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -42,6 +42,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: false, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+      aggregateTimeout: 300,
     }
   },
   plugins: [
